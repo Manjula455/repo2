a7a7e6384a5482c52d8a71d4e9cca40c9e69cb7f
Manjula.V <manjulavenkatappa57@gmail.com>
2025-05-16 19:02:25 +0530

    paregraph is added

--- countUseReducer/src/App.jsx
+++ countUseReducer/src/App.jsx
@@ -6,6 +6,12 @@ function App() {
   return (
     <>
       <h1>Counter Using Reducer</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci rerum
+        quo aliquid doloribus vel obcaecati minus, mollitia consequuntur quos
+        ipsam. Voluptate voluptatem fugiat eaque veritatis maxime vitae illo
+        doloribus incidunt?
+      </p>
     </>
   );
 }
